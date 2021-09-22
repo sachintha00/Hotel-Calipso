d55c1223d4fd051efeca1fd75fbcc4174808a84f
sachintha00 <sachintha0603@gmail.com>
2021-09-22 10:35:43 +0530

    updated

--- HotelCalipso/HotelCalipso/frmFood.Designer.cs
+++ HotelCalipso/HotelCalipso/frmFood.Designer.cs
@@ -32,6 +32,28 @@
             this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.imgFood1 = new System.Windows.Forms.PictureBox();
+            this.imgFood2 = new System.Windows.Forms.PictureBox();
+            this.imgFood3 = new System.Windows.Forms.PictureBox();
+            this.imgFood4 = new System.Windows.Forms.PictureBox();
+            this.txtPrice = new Guna.UI.WinForms.GunaTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtQuantity = new Guna.UI.WinForms.GunaTextBox();
+            this.lblQuantty = new Guna.UI.WinForms.GunaLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblBill = new System.Windows.Forms.Label();
+            this.btnSUbmit = new Guna.UI.WinForms.GunaButton();
+            this.btnClear = new Guna.UI.WinForms.GunaButton();
+            ((System.ComponentModel.ISupportInitialize)(this.imgFood1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgFood2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgFood3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgFood4)).BeginInit();
             this.SuspendLayout();
             // 
             // bunifuElipse1
@@ -60,17 +82,268 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "FOODS";
             // 
+            // imgFood1
+            // 
+            this.imgFood1.Location = new System.Drawing.Point(42, 139);
+            this.imgFood1.Name = "imgFood1";
+            this.imgFood1.Size = new System.Drawing.Size(172, 163);
+            this.imgFood1.TabIndex = 4;
+            this.imgFood1.TabStop = false;
+            this.imgFood1.Click += new System.EventHandler(this.imgFood1_Click);
+            // 
+            // imgFood2
+            // 
+            this.imgFood2.Location = new System.Drawing.Point(264, 139);
+            this.imgFood2.Name = "imgFood2";
+            this.imgFood2.Size = new System.Drawing.Size(172, 163);
+            this.imgFood2.TabIndex = 4;
+            this.imgFood2.TabStop = false;
+            this.imgFood2.Click += new System.EventHandler(this.imgFood2_Click);
+            // 
+            // imgFood3
+            // 
+            this.imgFood3.Location = new System.Drawing.Point(486, 139);
+            this.imgFood3.Name = "imgFood3";
+            this.imgFood3.Size = new System.Drawing.Size(172, 163);
+            this.imgFood3.TabIndex = 4;
+            this.imgFood3.TabStop = false;
+            this.imgFood3.Click += new System.EventHandler(this.imgFood3_Click);
+            // 
+            // imgFood4
+            // 
+            this.imgFood4.Location = new System.Drawing.Point(708, 139);
+            this.imgFood4.Name = "imgFood4";
+            this.imgFood4.Size = new System.Drawing.Size(172, 163);
+            this.imgFood4.TabIndex = 4;
+            this.imgFood4.TabStop = false;
+            this.imgFood4.Click += new System.EventHandler(this.imgFood4_Click);
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.BaseColor = System.Drawing.Color.White;
+            this.txtPrice.BorderColor = System.Drawing.Color.Silver;
+            this.txtPrice.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtPrice.Enabled = false;
+            this.txtPrice.FocusedBaseColor = System.Drawing.Color.White;
+            this.txtPrice.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(24)))), ((int)(((byte)(55)))));
+            this.txtPrice.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtPrice.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtPrice.Location = new System.Drawing.Point(42, 445);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.PasswordChar = '\0';
+            this.txtPrice.SelectedText = "";
+            this.txtPrice.Size = new System.Drawing.Size(263, 44);
+            this.txtPrice.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(36)))), ((int)(((byte)(83)))));
+            this.label3.Location = new System.Drawing.Point(38, 407);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(36)))), ((int)(((byte)(83)))));
+            this.label4.Location = new System.Drawing.Point(38, 505);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 24);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Quantity";
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.BaseColor = System.Drawing.Color.White;
+            this.txtQuantity.BorderColor = System.Drawing.Color.Silver;
+            this.txtQuantity.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtQuantity.FocusedBaseColor = System.Drawing.Color.White;
+            this.txtQuantity.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(24)))), ((int)(((byte)(55)))));
+            this.txtQuantity.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtQuantity.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtQuantity.Location = new System.Drawing.Point(42, 543);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.PasswordChar = '\0';
+            this.txtQuantity.SelectedText = "";
+            this.txtQuantity.Size = new System.Drawing.Size(263, 44);
+            this.txtQuantity.TabIndex = 11;
+            this.txtQuantity.TextChanged += new System.EventHandler(this.txtQuantity_TextChanged);
+            // 
+            // lblQuantty
+            // 
+            this.lblQuantty.AutoSize = true;
+            this.lblQuantty.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.lblQuantty.ForeColor = System.Drawing.Color.Red;
+            this.lblQuantty.Location = new System.Drawing.Point(42, 591);
+            this.lblQuantty.Name = "lblQuantty";
+            this.lblQuantty.Size = new System.Drawing.Size(0, 20);
+            this.lblQuantty.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(36)))), ((int)(((byte)(83)))));
+            this.label5.Location = new System.Drawing.Point(99, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Price";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(36)))), ((int)(((byte)(83)))));
+            this.label6.Location = new System.Drawing.Point(321, 316);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 24);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Price";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(36)))), ((int)(((byte)(83)))));
+            this.label7.Location = new System.Drawing.Point(543, 316);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 24);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Price";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(36)))), ((int)(((byte)(83)))));
+            this.label8.Location = new System.Drawing.Point(765, 316);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 24);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Price";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(36)))), ((int)(((byte)(83)))));
+            this.label9.Location = new System.Drawing.Point(451, 465);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(218, 24);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Your Total Bill Amount";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(36)))), ((int)(((byte)(83)))));
+            this.label10.Location = new System.Drawing.Point(445, 519);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(125, 58);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "RS. ";
+            // 
+            // lblBill
+            // 
+            this.lblBill.AutoSize = true;
+            this.lblBill.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBill.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(36)))), ((int)(((byte)(83)))));
+            this.lblBill.Location = new System.Drawing.Point(557, 519);
+            this.lblBill.Name = "lblBill";
+            this.lblBill.Size = new System.Drawing.Size(83, 58);
+            this.lblBill.TabIndex = 10;
+            this.lblBill.Text = "00";
+            // 
+            // btnSUbmit
+            // 
+            this.btnSUbmit.AnimationHoverSpeed = 0.07F;
+            this.btnSUbmit.AnimationSpeed = 0.03F;
+            this.btnSUbmit.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(36)))), ((int)(((byte)(83)))));
+            this.btnSUbmit.BorderColor = System.Drawing.Color.Black;
+            this.btnSUbmit.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnSUbmit.FocusedColor = System.Drawing.Color.Empty;
+            this.btnSUbmit.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.btnSUbmit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(20)))));
+            this.btnSUbmit.Image = null;
+            this.btnSUbmit.ImageSize = new System.Drawing.Size(20, 20);
+            this.btnSUbmit.Location = new System.Drawing.Point(42, 638);
+            this.btnSUbmit.Name = "btnSUbmit";
+            this.btnSUbmit.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(36)))), ((int)(((byte)(83)))));
+            this.btnSUbmit.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.btnSUbmit.OnHoverForeColor = System.Drawing.Color.White;
+            this.btnSUbmit.OnHoverImage = null;
+            this.btnSUbmit.OnPressedColor = System.Drawing.Color.Black;
+            this.btnSUbmit.Size = new System.Drawing.Size(263, 62);
+            this.btnSUbmit.TabIndex = 13;
+            this.btnSUbmit.Text = "BILL";
+            this.btnSUbmit.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.btnSUbmit.Click += new System.EventHandler(this.btnSUbmit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.AnimationHoverSpeed = 0.07F;
+            this.btnClear.AnimationSpeed = 0.03F;
+            this.btnClear.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(36)))), ((int)(((byte)(83)))));
+            this.btnClear.BorderColor = System.Drawing.Color.Black;
+            this.btnClear.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnClear.FocusedColor = System.Drawing.Color.Empty;
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.btnClear.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(20)))));
+            this.btnClear.Image = null;
+            this.btnClear.ImageSize = new System.Drawing.Size(20, 20);
+            this.btnClear.Location = new System.Drawing.Point(455, 638);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(36)))), ((int)(((byte)(83)))));
+            this.btnClear.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.btnClear.OnHoverForeColor = System.Drawing.Color.White;
+            this.btnClear.OnHoverImage = null;
+            this.btnClear.OnPressedColor = System.Drawing.Color.Black;
+            this.btnClear.Size = new System.Drawing.Size(203, 62);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "CLEAR TOTAL";
+            this.btnClear.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
             // frmFood
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(954, 650);
+            this.ClientSize = new System.Drawing.Size(954, 746);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSUbmit);
+            this.Controls.Add(this.lblQuantty);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblBill);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.imgFood4);
+            this.Controls.Add(this.imgFood3);
+            this.Controls.Add(this.imgFood2);
+            this.Controls.Add(this.imgFood1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmFood";
             this.Text = "frmRegister";
             this.Load += new System.EventHandler(this.frmFood_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgFood1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgFood2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgFood3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgFood4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,5 +354,23 @@
         private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox imgFood4;
+        private System.Windows.Forms.PictureBox imgFood3;
+        private System.Windows.Forms.PictureBox imgFood2;
+        private System.Windows.Forms.PictureBox imgFood1;
+        private Guna.UI.WinForms.GunaLabel lblQuantty;
+        private Guna.UI.WinForms.GunaTextBox txtQuantity;
+        private System.Windows.Forms.Label label4;
+        private Guna.UI.WinForms.GunaTextBox txtPrice;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblBill;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI.WinForms.GunaButton btnSUbmit;
+        private Guna.UI.WinForms.GunaButton btnClear;
     }
 }
--- HotelCalipso/HotelCalipso/frmFood.cs
+++ HotelCalipso/HotelCalipso/frmFood.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SMDValidation;
 
 namespace HotelCalipso
 {
@@ -20,6 +21,47 @@ namespace HotelCalipso
         private void frmFood_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void imgFood1_Click(object sender, EventArgs e)
+        {
+            txtPrice.Text = "240";
+        }
+
+        private void imgFood2_Click(object sender, EventArgs e)
+        {
+            txtPrice.Text = "300";
+        }
+
+        private void imgFood3_Click(object sender, EventArgs e)
+        {
+            txtPrice.Text = "350";
+        }
+
+        private void imgFood4_Click(object sender, EventArgs e)
+        {
+            txtPrice.Text = "600";
+        }
+
+        private void btnSUbmit_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtQuantity.Text))
+                Validation.texBoxValidate(false, txtQuantity, lblQuantty, "Error");
+            else
+                lblBill.Text = (Convert.ToDouble(txtPrice.Text) * Convert.ToDouble(txtQuantity.Text)).ToString();
+        }
+
+        private void txtQuantity_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtQuantity.Text))
+                Validation.texBoxValidate(true, txtQuantity, lblQuantty);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtPrice.Text = "";
+            txtQuantity.Text = "";
+            lblBill.Text = "";
         }
     }
 }
